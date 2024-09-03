a38fc6cb0139fbae7116b7b1c4b89e2963c74dc5
MJDM23 <42447436+MJDM23@users.noreply.github.com>
2024-09-03 17:02:36 -0500

    docs: agrega documentación de endpoints

--- prueba_docker/main.py
+++ prueba_docker/main.py
@@ -5,6 +5,24 @@ app = FastAPI()
 
 @app.get("/lista-ordenada")
 def lista_ordenada(lista_no_ordenada: str = Query(..., alias="lista-no-ordenada")):
+    """ Endpoint que recibe una lista no ordenada y devuelve la lista ordenada junto
+    con la hora del sistema.
+
+    Args:
+        lista_no_ordenada: Lista de números no ordenados en formato string.
+
+    Returns:
+        Un diccionario con la hora del sistema y la lista ordenada.
+
+    Examples:
+        .. code-block:: python
+
+            {
+                "hora_sistema": "2024-09-03 21:51:23",
+                "lista_ordenada": [1, 2, 3, 3, 6]
+            }
+
+    """
     lista_no_ordenada = lista_no_ordenada.strip('[]').split(',')
     lista_no_ordenada = [int(i) for i in lista_no_ordenada]
     lista_ordenada = sorted(lista_no_ordenada)
@@ -20,6 +38,17 @@ def lista_ordenada(lista_no_ordenada: str = Query(..., alias="lista-no-ordenada"
 
 @app.get("/healthcheck")
 def healthcheck():
+    """ Endpoint para verificar el estado de salud del servicio.
+
+    Returns:
+        Un string que indica el estado de salud del servicio.
+
+    Examples:
+        .. code-block:: python
+
+            "OK"
+
+    """
     respuesta = "OK"
 
     return respuesta
