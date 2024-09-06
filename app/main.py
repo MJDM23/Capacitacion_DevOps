8f8c989f9ed0271af0cb71c2bbdfdd3b8f6acc6c
MJDM23 <42447436+MJDM23@users.noreply.github.com>
2024-09-06 11:43:43 -0500

    feat: añade endpoint para guardar listas no ordenadas

--- app/main.py
+++ app/main.py
@@ -1,7 +1,55 @@
+# External libraries
+import uuid
 from fastapi import FastAPI, Query
 from datetime import datetime
 
+# Own libraries
+from config_mongo import get_mongo_db
+
 app = FastAPI()
+
+
+@app.get("/guardar-lista-no-ordenada")
+def guardar_lista_no_ordenada(
+    lista_no_ordenada: str = Query(..., alias="lista-no-ordenada")
+):
+    """Endpoint que recibe una lista no ordenada, la guarda en MongoDB y devuelve un
+    mensaje con el ID asignado.
+
+    Args:
+        lista_no_ordenada: Lista de números no ordenados en formato string.
+
+    Returns:
+        Un diccionario con un mensaje que incluye el ID único generado.
+
+    Examples:
+        .. code-block:: python
+
+            {
+                "msg": "La lista no ordenada fue guardada con el id: 9743ee94-6690-11ef-a4d5-089df4cb467e"
+            }
+
+    #noqa
+    """
+    lista_no_ordenada = lista_no_ordenada.strip('[]').split(',')
+    lista_no_ordenada = [int(i) for i in lista_no_ordenada]
+
+    hora_sistema = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    id_unico = str(uuid.uuid4())
+
+    datos = {
+        "id": id_unico,
+        "hora_sistema": hora_sistema,
+        "lista_no_ordenada": lista_no_ordenada,
+    }
+
+    db = get_mongo_db()
+    coleccion = db.listas_no_ordenadas
+    coleccion.insert_one(datos)
+
+    respuesta = {"msg": f"La lista no ordenada fue guardada con el id: {id_unico}"}
+
+    return respuesta
 
 
 @app.get("/healthcheck")
